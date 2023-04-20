ae3ec43fd08881c87f78f79081f3ec930be3b5a8
BEN_HO <pohan.ho@gmail.com>
2023-04-20 16:07:29 +0800

    [dev] test

---
+++ tests/test_math.py
@@ -0,0 +1,19 @@
+import unittest
+from mymath import add, subtract, multiply, divide
+
+class TestMath(unittest.TestCase):
+    def test_add(self):
+        self.assertEqual(add(1, 2), 4)
+
+    def test_subtract(self):
+        self.assertEqual(subtract(1, 2), -1)
+
+    def test_multiply(self):
+        self.assertEqual(multiply(1, 2), 2)
+
+    def test_divide(self):
+        self.assertEqual(divide(1, 2), 0.5)
+
+if __name__ == '__main__':
+    unittest.main()
+
